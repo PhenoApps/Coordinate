44bfde7f4e48e58c79b811f49f34dac169c7c30a
Peter Rosario <PeterRosario@ksu.edu>
2017-09-15 16:33:23 -0500

    ExtraNewTemplateAlertDialog: Turned configure() into makeBuilder() since what it did only had to happen once initially, not every time show() is called.

--- app/src/main/java/org/wheatgenetics/coordinate/ui/ExtraNewTemplateAlertDialog.java
+++ app/src/main/java/org/wheatgenetics/coordinate/ui/ExtraNewTemplateAlertDialog.java
@@ -38,9 +38,9 @@ class ExtraNewTemplateAlertDialog extends org.wheatgenetics.coordinate.ui.Showin
     { super(activity); this.handler = handler; }
 
     @java.lang.Override
-    android.app.AlertDialog.Builder configure(final int titleId)
+    android.app.AlertDialog.Builder makeBuilder(final int titleId)
     {
-        super.configure(titleId);
+        super.makeBuilder(titleId);
 
         {
             final android.view.View view =
