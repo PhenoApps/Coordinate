740d6febb86640fd36e2e51ed9ccfd32c2733422
Peter Rosario <PeterRosario@ksu.edu>
2017-12-12 07:58:40 -0600

    TemplateModels: Added.

---
+++ app/src/androidTest/java/org/wheatgenetics/coordinate/model/TemplateModelsTest.java
@@ -0,0 +1,85 @@
+package org.wheatgenetics.coordinate.model;
+
+/**
+ * Uses:
+ * org.junit.Assert
+ * org.junit.Test
+ *
+ * org.wheatgenetics.coordinate.model.TemplateModel
+ * org.wheatgenetics.coordinate.model.TemplateModels
+ */
+@java.lang.SuppressWarnings("ClassExplicitlyExtendsObject")
+public class TemplateModelsTest extends java.lang.Object
+{
+    @org.junit.Test
+    public void iteratorWorks()
+    {
+        int count = 0;
+        final org.wheatgenetics.coordinate.model.TemplateModels templateModels =
+            org.wheatgenetics.coordinate.model.TemplateModels.makeDefault();
+        for (final org.wheatgenetics.coordinate.model.TemplateModel templateModel: templateModels)
+            count++;
+        org.junit.Assert.assertEquals(templateModels.size(), count);
+    }
+
+    // region add() Tests
+    @org.junit.Test
+    public void duplicateAddFails()
+    {
+        final org.wheatgenetics.coordinate.model.TemplateModels templateModels =
+            org.wheatgenetics.coordinate.model.TemplateModels.makeDefault();
+        templateModels.add(org.wheatgenetics.coordinate.model.TemplateModel.makeDNADefault());
+        org.junit.Assert.assertEquals(templateModels.size(), 2);
+    }
+
+    @org.junit.Test
+    public void addSucceeds()
+    {
+        final org.wheatgenetics.coordinate.model.TemplateModels templateModels =
+            org.wheatgenetics.coordinate.model.TemplateModels.makeDefault();
+        templateModels.add(new org.wheatgenetics.coordinate.model.TemplateModel(
+            /* id             => */ 0          ,
+            /* title          => */ "testTitle",
+            /* code           => */ 1          ,
+            /* rows           => */ 5          ,
+            /* cols           => */ 2          ,
+            /* excludeCells   => */ null       ,
+            /* excludeRows    => */ null       ,
+            /* excludeCols    => */ null       ,
+            /* colNumbering   => */ 1          ,
+            /* rowNumbering   => */ 0          ,
+            /* optionalFields => */ null       ,
+            /* timestamp      => */ 0          ));
+        org.junit.Assert.assertEquals(templateModels.size(), 3);
+    }
+    // endregion
+
+    @org.junit.Test
+    public void makeDefaultAndSizeSucceed()
+    {
+        org.junit.Assert.assertEquals(
+            org.wheatgenetics.coordinate.model.TemplateModels.makeDefault().size(), 2);
+    }
+
+    @org.junit.Test
+    public void makeDefaultAndTitlesSucceed()
+    {
+        final org.wheatgenetics.coordinate.model.TemplateModels templateModels =
+            org.wheatgenetics.coordinate.model.TemplateModels.makeDefault();
+        final java.lang.String titles[] = new java.lang.String[] {
+            org.wheatgenetics.coordinate.model.TemplateModel.makeSeedDefault().getTitle() ,
+            org.wheatgenetics.coordinate.model.TemplateModel.makeDNADefault ().getTitle() };
+        org.junit.Assert.assertArrayEquals(templateModels.titles(), titles);
+    }
+
+    @org.junit.Test
+    public void makeDefaultAndGetSucceed()
+    {
+        final org.wheatgenetics.coordinate.model.TemplateModels templateModels =
+            org.wheatgenetics.coordinate.model.TemplateModels.makeDefault();
+        org.junit.Assert.assertTrue(templateModels.get(0).equals(
+            org.wheatgenetics.coordinate.model.TemplateModel.makeSeedDefault()));
+        org.junit.Assert.assertTrue(templateModels.get(1).equals(
+            org.wheatgenetics.coordinate.model.TemplateModel.makeDNADefault()));
+    }
+}
