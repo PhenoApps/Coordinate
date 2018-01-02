60f674dc7334059b4b3c2e30622d2e1ca1a79c4a
Peter Rosario <PeterRosario@ksu.edu>
2018-01-02 12:08:00 -0600

    GridCreator: Added.

---
+++ app/src/main/java/org/wheatgenetics/coordinate/gc/GridCreator.java
@@ -0,0 +1,207 @@
+package org.wheatgenetics.coordinate.gc;
+
+/**
+ * Uses:
+ * android.app.Activity
+ *
+ * org.wheatgenetics.coordinate.R
+ * org.wheatgenetics.coordinate.SelectAlertDialog
+ * org.wheatgenetics.coordinate.SelectAlertDialog.Handler
+ * org.wheatgenetics.coordinate.Utils
+ *
+ * org.wheatgenetics.coordinate.database.GridsTable
+ * org.wheatgenetics.coordinate.database.TemplatesTable
+ *
+ * org.wheatgenetics.coordinate.model.JoinedGridModel
+ * org.wheatgenetics.coordinate.model.TemplateModel
+ * org.wheatgenetics.coordinate.model.TemplateModels
+ *
+ * org.wheatgenetics.coordinate.optionalField.CheckedOptionalFields
+ * org.wheatgenetics.coordinate.optionalField.NonNullOptionalFields
+ *
+ * org.wheatgenetics.coordinate.tc.TemplateCreator
+ * org.wheatgenetics.coordinate.tc.TemplateCreator.Handler
+ *
+ * org.wheatgenetics.coordinate.gc.GetTemplateChoiceAlertDialog
+ * org.wheatgenetics.coordinate.gc.GetTemplateChoiceAlertDialog.Handler
+ * org.wheatgenetics.coordinate.gc.SetOptionalFieldValuesAlertDialog
+ * org.wheatgenetics.coordinate.gc.SetOptionalFieldValuesAlertDialog.Handler
+ */
+@java.lang.SuppressWarnings("ClassExplicitlyExtendsObject")
+public class GridCreator extends java.lang.Object implements
+org.wheatgenetics.coordinate.gc.GetTemplateChoiceAlertDialog.Handler,
+org.wheatgenetics.coordinate.SelectAlertDialog.Handler,
+org.wheatgenetics.coordinate.tc.TemplateCreator.Handler,
+org.wheatgenetics.coordinate.gc.SetOptionalFieldValuesAlertDialog.Handler
+{
+    @java.lang.SuppressWarnings("UnnecessaryInterfaceModifier")
+    interface Handler
+    {
+        public abstract void setPerson        (java.lang.String person);
+        public abstract void handleGridCreated(
+            org.wheatgenetics.coordinate.model.JoinedGridModel joinedGridModel);
+    }
+
+    // region Fields
+    private final android.app.Activity                                activity;
+    private final org.wheatgenetics.coordinate.gc.GridCreator.Handler handler ;
+
+    private org.wheatgenetics.coordinate.database.GridsTable gridsTableInstance = null;
+
+    private org.wheatgenetics.coordinate.optionalField.NonNullOptionalFields  optionalFields;
+    private org.wheatgenetics.coordinate.gc.SetOptionalFieldValuesAlertDialog
+        setOptionalFieldValuesAlertDialog = null;
+
+    private org.wheatgenetics.coordinate.model.TemplateModel templateModel;
+
+    private org.wheatgenetics.coordinate.SelectAlertDialog       chooseOldAlertDialog   = null;
+    private org.wheatgenetics.coordinate.database.TemplatesTable templatesTableInstance = null;
+    private org.wheatgenetics.coordinate.model.TemplateModels    templateModels         = null;
+
+    private org.wheatgenetics.coordinate.tc.TemplateCreator templateCreator = null;
+
+    private org.wheatgenetics.coordinate.gc.GetTemplateChoiceAlertDialog
+        getTemplateChoiceAlertDialog = null;
+    // endregion
+
+    // region Private Methods
+    private org.wheatgenetics.coordinate.database.TemplatesTable templatesTable()
+    {
+        if (null == this.templatesTableInstance) this.templatesTableInstance =
+            new org.wheatgenetics.coordinate.database.TemplatesTable(this.activity);
+        return this.templatesTableInstance;
+    }
+
+    private void setValues()
+    {
+        assert null != this.templateModel;
+        if (this.templateModel.optionalFieldsIsEmpty())
+        {
+            this.optionalFields = null;
+            this.handleSetValuesDone();
+        }
+        else
+        {
+            this.optionalFields = this.templateModel.optionalFieldsClone();
+            if (null == this.setOptionalFieldValuesAlertDialog)
+                this.setOptionalFieldValuesAlertDialog =
+                    new org.wheatgenetics.coordinate.gc.SetOptionalFieldValuesAlertDialog(
+                        this.activity, this);
+            this.setOptionalFieldValuesAlertDialog.show(this.templateModel.getTitle(),
+                new org.wheatgenetics.coordinate.optionalField.CheckedOptionalFields(
+                    this.optionalFields),
+                /* firstCannotBeEmpty => */ this.templateModel.getType().isDefaultTemplate()); // TODO: Hardcode?
+        }
+    }
+
+    private org.wheatgenetics.coordinate.database.GridsTable gridsTable()
+    {
+        if (null == this.gridsTableInstance) this.gridsTableInstance =
+            new org.wheatgenetics.coordinate.database.GridsTable(this.activity);
+        return this.gridsTableInstance;
+    }
+    // endregion
+
+    public GridCreator(final android.app.Activity activity,
+    final org.wheatgenetics.coordinate.gc.GridCreator.Handler handler)
+    { super(); this.activity = activity; this.handler = handler; }
+
+    // region Overridden Methods
+    // region org.wheatgenetics.coordinate.gc.SetOptionalFieldValuesAlertDialog.Handler Overridden Methods
+    @java.lang.Override
+    public void setPerson(final java.lang.String person)
+    { assert null != this.handler; this.handler.setPerson(person); }
+
+    @java.lang.Override
+    public void handleSetValuesDone()
+    {
+        final org.wheatgenetics.coordinate.model.JoinedGridModel joinedGridModel;
+        {
+            final long gridId = this.gridsTable().insert(
+                new org.wheatgenetics.coordinate.model.JoinedGridModel(
+                    /* optionalFields => */ this.optionalFields,
+                    /* templateModel  => */ this.templateModel));
+            this.optionalFields = null; this.templateModel = null;
+
+            if (gridId <= 0)
+            {
+                joinedGridModel = null;
+                org.wheatgenetics.coordinate.Utils.alert(this.activity,
+                    org.wheatgenetics.coordinate.R.string.GridCreatorGridAlertMessage);
+            }
+            else joinedGridModel = this.gridsTable().get(gridId);
+        }
+        assert null != this.handler; this.handler.handleGridCreated(joinedGridModel);
+
+    }
+    // endregion
+
+    // region org.wheatgenetics.coordinate.SelectAlertDialog.Handler Overridden Method
+    @java.lang.Override
+    public void select(final int which)
+    {
+        assert null != this.templateModels; this.templateModel = this.templateModels.get(which);
+        this.templateModels = null;
+        this.setValues();
+    }
+    // endregion
+
+    // region org.wheatgenetics.coordinate.tc.TemplateCreator.Handler Overridden Method
+    @java.lang.Override
+    public void handleTemplateCreated(
+    final org.wheatgenetics.coordinate.model.TemplateModel templateModel)
+    {
+        this.templateModel = templateModel;
+
+        boolean inserted;
+        {
+            final long templateId = this.templatesTable().insert(this.templateModel);
+            if (templateId > 0)
+            {
+                assert null != this.templateModel; this.templateModel.setId(templateId);
+                inserted = true;
+            }
+            else
+            {
+                org.wheatgenetics.coordinate.Utils.alert(this.activity,
+                    org.wheatgenetics.coordinate.R.string.GridCreatorTemplateAlertMessage);
+                inserted = false;
+            }
+        }
+
+        if (inserted)
+            this.setValues();
+        else
+            { assert null != this.handler; this.handler.handleGridCreated(null); }
+    }
+    // endregion
+
+    // region org.wheatgenetics.coordinate.gc.GetTemplateChoiceAlertDialog.Handler Overridden Methods
+    @java.lang.Override
+    public void chooseOld()
+    {
+        if (null == this.chooseOldAlertDialog) this.chooseOldAlertDialog =
+            new org.wheatgenetics.coordinate.SelectAlertDialog(this.activity, this);
+        this.templateModels = this.templatesTable().load();
+        assert null != this.templateModels; this.chooseOldAlertDialog.show(
+            org.wheatgenetics.coordinate.R.string.ChooseOldAlertDialogTitle,
+            this.templateModels.titles()                                   );
+    }
+
+    @java.lang.Override
+    public void chooseNew()
+    {
+        if (null == this.templateCreator) this.templateCreator =
+            new org.wheatgenetics.coordinate.tc.TemplateCreator(this.activity, this);
+        this.templateCreator.create();
+    }
+    // endregion
+    // endregion
+
+    public void create()
+    {
+        if (null == this.getTemplateChoiceAlertDialog) this.getTemplateChoiceAlertDialog =
+            new org.wheatgenetics.coordinate.gc.GetTemplateChoiceAlertDialog(this.activity, this);
+        this.getTemplateChoiceAlertDialog.show();
+    }
+}
