2ad0550c8ee68a554d712cb3422cee56760cdda2
Peter Rosario <PeterRosario@ksu.edu>
2018-05-14 12:32:36 -0500

    Refactored.

--- app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
@@ -1,118 +1,124 @@
 package org.wheatgenetics.coordinate;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity
-    implements NavigationView.OnNavigationItemSelectedListener
+/**
+ * Uses:
+ * android.os.Bundle
+ * android.support.annotation.NonNull
+ * android.support.design.widget.FloatingActionButton
+ * android.support.design.widget.NavigationView
+ * android.support.design.widget.Snackbar
+ * android.support.v4.view.GravityCompat
+ * android.support.v4.widget.DrawerLayout
+ * android.support.v7.app.ActionBarDrawerToggle
+ * android.support.v7.app.AppCompatActivity
+ * android.support.v7.widget.Toolbar
+ * android.view.Menu
+ * android.view.MenuItem
+ * android.view.View
+ *
+ * org.wheatgenetics.coordinate.R
+ */
+public class MainActivity extends android.support.v7.app.AppCompatActivity
+implements android.support.design.widget.NavigationView.OnNavigationItemSelectedListener
 {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+    @java.lang.Override protected void onCreate(final android.os.Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        this.setContentView(org.wheatgenetics.coordinate.R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
+        // region Configure action bar.
         {
-            @Override
-            public void onClick(View view)
+            final android.support.v7.widget.Toolbar toolbar = this.findViewById(
+                org.wheatgenetics.coordinate.R.id.toolbar);         // From layout/app_bar_main.xml.
+            this.setSupportActionBar(toolbar);
+
             {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
+                final android.support.design.widget.FloatingActionButton fab = this.findViewById(
+                    org.wheatgenetics.coordinate.R.id.fab);         // From layout/app_bar_main.xml.
+                assert null != fab; fab.setOnClickListener(new android.view.View.OnClickListener()
+                    {
+                        @java.lang.Override public void onClick(final android.view.View view)
+                        {
+                            android.support.design.widget.Snackbar.make(
+                                    view, "Replace with your own action",
+                                    android.support.design.widget.Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                        }
+                    });
             }
-        });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed()
-    {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START))
-        {
-            drawer.closeDrawer(GravityCompat.START);
-        } else
-        {
-            super.onBackPressed();
+            final android.support.v4.widget.DrawerLayout drawerLayout = this.findViewById(
+                org.wheatgenetics.coordinate.R.id.drawer_layout);  // From layout/activity_main.xml.
+            final android.support.v7.app.ActionBarDrawerToggle toggle =
+                new android.support.v7.app.ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                    org.wheatgenetics.coordinate.R.string.navigation_drawer_open ,
+                    org.wheatgenetics.coordinate.R.string.navigation_drawer_close);
+            assert null != drawerLayout; drawerLayout.setDrawerListener(toggle);
+            toggle.syncState();
         }
+        // endregion
+
+        // region Configure navigation view.
+        final android.support.design.widget.NavigationView navigationView = this.findViewById(
+            org.wheatgenetics.coordinate.R.id.nav_view);           // From layout/activity_main.xml.
+        assert null != navigationView; navigationView.setNavigationItemSelectedListener(this);
+        // endregion
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
+    @java.lang.Override public void onBackPressed()
+    {
+        final android.support.v4.widget.DrawerLayout drawerLayout = this.findViewById(
+            org.wheatgenetics.coordinate.R.id.drawer_layout);      // From layout/activity_main.xml.
+        assert null != drawerLayout;
+        if (drawerLayout.isDrawerOpen(android.support.v4.view.GravityCompat.START))
+            drawerLayout.closeDrawer(android.support.v4.view.GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    @java.lang.Override public boolean onCreateOptionsMenu(final android.view.Menu menu)
     {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        this.getMenuInflater().inflate(org.wheatgenetics.coordinate.R.menu.main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
+    @java.lang.Override public boolean onOptionsItemSelected(final android.view.MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // Handle action bar item clicks here.  The action bar will automatically handle clicks on
+        // the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml.
+        assert null != item; final int itemId = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
+        if (org.wheatgenetics.coordinate.R.id.action_settings == itemId)
             return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        else
+            return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item)
+    // region android.support.design.widget.NavigationView.OnNavigationItemSelectedListener Overridden Method
+    @java.lang.Override @java.lang.SuppressWarnings({"StatementWithEmptyBody"})
+    public boolean onNavigationItemSelected(
+    @android.support.annotation.NonNull final android.view.MenuItem item)
     {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera)
+        switch (item.getItemId())
         {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery)
-        {
-
-        } else if (id == R.id.nav_slideshow)
-        {
-
-        } else if (id == R.id.nav_manage)
-        {
-
-        } else if (id == R.id.nav_share)
-        {
-
-        } else if (id == R.id.nav_send)
-        {
-
+            case org.wheatgenetics.coordinate.R.id.nav_camera   : break;
+            case org.wheatgenetics.coordinate.R.id.nav_gallery  : break;
+            case org.wheatgenetics.coordinate.R.id.nav_slideshow: break;
+            case org.wheatgenetics.coordinate.R.id.nav_manage   : break;
+            case org.wheatgenetics.coordinate.R.id.nav_share    : break;
+            case org.wheatgenetics.coordinate.R.id.nav_send     : break;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        {
+            final android.support.v4.widget.DrawerLayout drawerLayout = this.findViewById(
+                org.wheatgenetics.coordinate.R.id.drawer_layout);  // From layout/activity_main.xml.
+            assert null != drawerLayout;
+            drawerLayout.closeDrawer(android.support.v4.view.GravityCompat.START);
+        }
         return true;
     }
+    // endregion
 }
