04083ff385dc5f31d3f8462c99d9c9a920916e98
Peter Rosario <PeterRosario@ksu.edu>
2018-05-15 16:04:01 -0500

    Added org.wheatgenetics.coordinate.database.

---
+++ app/src/androidTest/java/org/wheatgenetics/coordinate/database/TableTest.java
@@ -0,0 +1,174 @@
+package org.wheatgenetics.coordinate.database;
+
+/**
+ * Uses:
+ * android.database.Cursor
+ * android.support.test.InstrumentationRegistry
+ *
+ * org.junit.After
+ * org.junit.Assert
+ * org.junit.Test
+ *
+ * org.wheatgenetics.javalib.Utils
+ *
+ * _org.wheatgenetics.coordinate.model.Model
+ *
+ * org.wheatgenetics.coordinate.database.Table
+ *
+ * org.wheatgenetics.coordinate.database.DatabaseTest
+ */
+@java.lang.SuppressWarnings({"ClassExplicitlyExtendsObject"})
+public class TableTest extends java.lang.Object
+{
+    /**
+     * This class was defined in order to test Table.  Why not just test Table directly?  Because
+     * Table is abstract.  Why does that matter?  Because I can't instantiate an abstract class.  If
+     * I can't instantiate it I can't test it.
+     */
+    private static class ConcreteTable extends org.wheatgenetics.coordinate.database.Table
+    {
+        private ConcreteTable()
+        {
+            super(
+                /* context   => */ android.support.test.InstrumentationRegistry.getTargetContext(),
+                /* tableName => */ "sqlite_master"                                                ,
+                /* tag       => */ "TableTest"                                                    ,
+                /* databaseName => */
+                    org.wheatgenetics.coordinate.database.DatabaseTest.DATABASE_FILE_NAME);
+        }
+
+        @java.lang.Override
+        org.wheatgenetics.coordinate.model.Model make(android.database.Cursor cursor)
+        { return null; }
+    }
+
+    @org.junit.Test public void secondConstructorSucceeds()
+    { new org.wheatgenetics.coordinate.database.TableTest.ConcreteTable(); }
+
+    // region Package External Operation Tests
+    @org.junit.Test public void whereClauseSucceeds()
+    {
+        final long id = 45;
+        org.junit.Assert.assertEquals(
+            org.wheatgenetics.coordinate.database.Table.ID_FIELD_NAME + " = " + id,
+            org.wheatgenetics.coordinate.database.Table.whereClause(id)           );
+    }
+
+    @org.junit.Test public void ascendingQueryAll()
+    {
+        final android.database.Cursor cursor =
+            new org.wheatgenetics.coordinate.database.TableTest.ConcreteTable().queryAll(
+                "[name] ASC");
+        org.junit.Assert.assertNotNull(cursor);
+        try
+        {
+            org.junit.Assert.assertEquals     (cursor.getColumnCount(), 5);
+            org.junit.Assert.assertArrayEquals(
+                new java.lang.String[]{"type", "name", "tbl_name", "rootpage", "sql"},
+                cursor.getColumnNames()                                              );
+            org.junit.Assert.assertEquals(cursor.getCount(), 8);
+
+            final java.lang.String firstRowName, lastRowName;
+            {
+                final int nameColumnIndex = 1;
+                cursor.moveToFirst(); firstRowName = cursor.getString(nameColumnIndex);
+                cursor.moveToLast (); lastRowName  = cursor.getString(nameColumnIndex);
+            }
+            org.junit.Assert.assertTrue(firstRowName.compareTo(lastRowName) < 0);
+        }
+        finally { cursor.close(); }
+    }
+
+    @org.junit.Test public void descendingQueryAll()
+    {
+        final android.database.Cursor cursor =
+            new org.wheatgenetics.coordinate.database.TableTest.ConcreteTable().queryAll(
+                /* orderBy => */ "[name] DESC");
+        org.junit.Assert.assertNotNull(cursor);
+        try
+        {
+            final java.lang.String firstRowName, lastRowName;
+            {
+                final int nameColumnIndex = 1;
+                cursor.moveToFirst(); firstRowName = cursor.getString(nameColumnIndex);
+                cursor.moveToLast (); lastRowName  = cursor.getString(nameColumnIndex);
+            }
+            org.junit.Assert.assertTrue(firstRowName.compareTo(lastRowName) > 0);
+        }
+        finally { cursor.close(); }
+    }
+
+    @org.junit.Test public void rawQuerySucceeds()
+    {
+        final android.database.Cursor firstCursor, secondCursor;
+        {
+            final org.wheatgenetics.coordinate.database.TableTest.ConcreteTable concreteTable =
+                new org.wheatgenetics.coordinate.database.TableTest.ConcreteTable();
+            firstCursor = concreteTable.rawQuery(
+                "SELECT ALL [sql] FROM [sqlite_master] WHERE [name] = 'templates'");
+            secondCursor = concreteTable.rawQuery(
+                "SELECT ALL [sql] FROM [sqlite_master] WHERE [name] = ?",
+                org.wheatgenetics.javalib.Utils.stringArray("templates"));
+        }
+        org.junit.Assert.assertNotNull(firstCursor); org.junit.Assert.assertNotNull(secondCursor);
+
+        try
+        {
+            org.junit.Assert.assertEquals(firstCursor.getCount (), 1);
+            org.junit.Assert.assertEquals(secondCursor.getCount(), 1);
+
+            firstCursor.moveToFirst(); secondCursor.moveToFirst();
+            org.junit.Assert.assertEquals(firstCursor.getString(0), secondCursor.getString(0));
+        }
+        finally { firstCursor.close(); secondCursor.close(); }
+    }
+
+    @org.junit.Test public void queryDistinctSucceeds()
+    {
+        final org.wheatgenetics.coordinate.database.TableTest.ConcreteTable concreteTable =
+            new org.wheatgenetics.coordinate.database.TableTest.ConcreteTable();
+        {
+            final android.database.Cursor cursor = concreteTable.rawQuery(
+                "SELECT ALL * FROM [sqlite_master] WHERE [type] = 'table'");
+            org.junit.Assert.assertNotNull(cursor);
+            try     { org.junit.Assert.assertEquals(6, cursor.getCount()); }
+            finally { cursor.close()                                     ; }
+        }
+
+        {
+            final android.database.Cursor cursor = concreteTable.queryDistinct("[type] = 'table'");
+            org.junit.Assert.assertNotNull(cursor);
+            try     { org.junit.Assert.assertEquals(6, cursor.getCount()); }
+            finally { cursor.close()                                     ; }
+        }
+    }
+
+    @org.junit.Test public void existsFailsAndSucceeds()
+    {
+        org.junit.Assert.assertFalse(org.wheatgenetics.coordinate.database.Table.exists(null));
+
+        final android.database.Cursor cursor =
+            new org.wheatgenetics.coordinate.database.TableTest.ConcreteTable().queryAll(null);
+        org.junit.Assert.assertNotNull(cursor);
+        try
+        { org.junit.Assert.assertTrue(org.wheatgenetics.coordinate.database.Table.exists(cursor)); }
+        finally { cursor.close(); }
+    }
+
+    @org.junit.Test public void nullCursorMakeFromFirstReturnsNull()
+    {
+        org.junit.Assert.assertNull(new
+            org.wheatgenetics.coordinate.database.TableTest.ConcreteTable().makeFromFirst(null));
+    }
+
+    @org.junit.Test public void getContentValuesForInsertSucceeds()
+    {
+        org.junit.Assert.assertNotNull(
+            new org.wheatgenetics.coordinate.database.TableTest.ConcreteTable()
+                .getContentValuesForInsert(null));
+    }
+    // endregion
+
+    @org.junit.After public void tearDown()
+    { new org.wheatgenetics.coordinate.database.DatabaseTest().cleanFilesystem(); }
+}
---
+++ app/src/main/java/org/wheatgenetics/coordinate/database/TemplatesTable.java
@@ -0,0 +1,178 @@
+package org.wheatgenetics.coordinate.database;
+
+/**
+ * Uses:
+ * android.content.ContentValues
+ * android.content.Context
+ * android.database.Cursor
+ *
+ * org.wheatgenetics.javalib.Utils
+ *
+ * org.wheatgenetics.coordinate.model.Model
+ * org.wheatgenetics.coordinate.model.TemplateModel
+ * org.wheatgenetics.coordinate.model.TemplateModels
+ * org.wheatgenetics.coordinate.model.TemplateType
+ *
+ * org.wheatgenetics.coordinate.database.Table
+ */
+public class TemplatesTable extends org.wheatgenetics.coordinate.database.Table
+{
+    // region Constants
+    static final java.lang.String TABLE_NAME = "templates";
+
+    static final java.lang.String TITLE_FIELD_NAME = "title", TYPE_FIELD_NAME   = "type"  ;
+    static final java.lang.String ROWS_FIELD_NAME  = "rows" , COLS_FIELD_NAME   = "cols"  ;
+    static final java.lang.String ERAND_FIELD_NAME = "erand", ECELLS_FIELD_NAME = "ecells",
+        EROWS_FIELD_NAME = "erows", ECOLS_FIELD_NAME = "ecols";
+    static final java.lang.String CNUMB_FIELD_NAME   = "cnumb"  , RNUMB_FIELD_NAME = "rnumb";
+    static final java.lang.String OPTIONS_FIELD_NAME = "options";
+    static final java.lang.String STAMP_FIELD_NAME   = "stamp"  ;
+    // endregion
+
+    // region Private Methods
+    private android.database.Cursor query(
+    final org.wheatgenetics.coordinate.model.TemplateType templateType)
+    {
+        assert null != templateType; return this.queryDistinct(/* selection => */
+            org.wheatgenetics.coordinate.database.TemplatesTable.TYPE_FIELD_NAME + " = " +
+                templateType.getCode());
+    }
+
+    private org.wheatgenetics.coordinate.model.TemplateModels makeTemplateModels(
+    final android.database.Cursor cursor)
+    {
+        final org.wheatgenetics.coordinate.model.TemplateModels result;
+        if (null == cursor)
+            result = null;
+        else
+            try
+            {
+                if (cursor.getCount() <= 0)
+                    result = null;
+                else
+                {
+                    result = new org.wheatgenetics.coordinate.model.TemplateModels();
+                    while (cursor.moveToNext()) result.add(
+                        (org.wheatgenetics.coordinate.model.TemplateModel) this.make(cursor));
+                }
+            }
+            finally { cursor.close(); }
+        return result;
+    }
+    // endregion
+
+    public TemplatesTable(final android.content.Context context)
+    {
+        super(
+            /* context   => */ context                                                        ,
+            /* tableName => */ org.wheatgenetics.coordinate.database.TemplatesTable.TABLE_NAME,
+            /* tag       => */ "TemplatesTable"                                               );
+    }
+
+    // region Overridden Methods
+    @java.lang.Override
+    org.wheatgenetics.coordinate.model.Model make(final android.database.Cursor cursor)
+    {
+        return null == cursor ? null : new org.wheatgenetics.coordinate.model.TemplateModel(
+            /* id => */ cursor.getLong(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.Table.ID_FIELD_NAME)),
+            /* title => */ cursor.getString(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.TITLE_FIELD_NAME)),
+            /* code => */ cursor.getInt(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.TYPE_FIELD_NAME)),
+            /* rows => */ cursor.getInt(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.ROWS_FIELD_NAME)),
+            /* cols => */ cursor.getInt(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.COLS_FIELD_NAME)),
+            /* generatedExcludedCellsAmount => */ cursor.getInt(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.ERAND_FIELD_NAME)),
+            /* excludeCells => */ cursor.getString(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.ECELLS_FIELD_NAME)),
+            /* excludeRows => */ cursor.getString(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.EROWS_FIELD_NAME)),
+            /* excludeCols => */ cursor.getString(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.ECOLS_FIELD_NAME)),
+            /* colNumbering => */ cursor.getInt(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.CNUMB_FIELD_NAME)),
+            /* rowNumbering => */ cursor.getInt(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.RNUMB_FIELD_NAME)),
+            /* optionalFields => */ cursor.getString(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.OPTIONS_FIELD_NAME)),
+            /* timestamp => */ cursor.getLong(cursor.getColumnIndex(
+                org.wheatgenetics.coordinate.database.TemplatesTable.STAMP_FIELD_NAME)));
+    }
+
+    @java.lang.Override
+    android.content.ContentValues getContentValuesForInsert(
+    final org.wheatgenetics.coordinate.model.Model model)
+    {
+        final android.content.ContentValues result = super.getContentValuesForInsert(model);
+        {
+            final org.wheatgenetics.coordinate.model.TemplateModel templateModel =
+                (org.wheatgenetics.coordinate.model.TemplateModel) model;
+
+            assert null != templateModel;
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.TITLE_FIELD_NAME,
+                templateModel.getTitle());
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.TYPE_FIELD_NAME,
+                templateModel.getType().getCode());
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.ROWS_FIELD_NAME,
+                templateModel.getRows());
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.COLS_FIELD_NAME,
+                templateModel.getCols());
+
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.ERAND_FIELD_NAME,
+                templateModel.getGeneratedExcludedCellsAmount());
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.ECELLS_FIELD_NAME,
+                templateModel.getExcludedCellsAsJson());
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.EROWS_FIELD_NAME,
+                templateModel.getExcludedRowsAsJson());
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.ECOLS_FIELD_NAME,
+                templateModel.getExcludedColsAsJson());
+
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.CNUMB_FIELD_NAME,
+                templateModel.getColNumbering());
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.RNUMB_FIELD_NAME,
+                templateModel.getRowNumbering());
+
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.OPTIONS_FIELD_NAME,
+                templateModel.optionalFieldsAsJson());
+
+            result.put(org.wheatgenetics.coordinate.database.TemplatesTable.STAMP_FIELD_NAME,
+                templateModel.getTimestamp());
+        }
+        return result;
+    }
+    // endregion
+
+    // region Operations
+    public boolean exists(final org.wheatgenetics.coordinate.model.TemplateType templateType)
+    { return org.wheatgenetics.coordinate.database.Table.exists(this.query(templateType)); }
+
+    public org.wheatgenetics.coordinate.model.TemplateModel get(final long id)
+    {
+        return (org.wheatgenetics.coordinate.model.TemplateModel) this.makeFromFirst(this.queryAll(
+            /* selection     => */ org.wheatgenetics.coordinate.database.Table.whereClause(),
+            /* selectionArgs => */ org.wheatgenetics.javalib.Utils.stringArray(id)          ));
+    }
+
+    public org.wheatgenetics.coordinate.model.TemplateModel get(
+    final org.wheatgenetics.coordinate.model.TemplateType templateType)
+    {
+        return (org.wheatgenetics.coordinate.model.TemplateModel)
+            this.makeFromFirst(this.query(templateType));
+    }
+
+    public org.wheatgenetics.coordinate.model.TemplateModels load()
+    {
+        return this.makeTemplateModels(this.queryAll(/* orderBy => */
+            org.wheatgenetics.coordinate.database.TemplatesTable.TYPE_FIELD_NAME + " ASC"));
+    }
+
+    public org.wheatgenetics.coordinate.model.TemplateModels loadUserDefined()
+    {
+        return this.makeTemplateModels(this.query(
+            org.wheatgenetics.coordinate.model.TemplateType.USERDEFINED));
+    }
+    // endregion
+}
