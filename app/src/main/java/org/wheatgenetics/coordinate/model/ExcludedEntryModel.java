806273b7a5f651a2ff1b544cd3afb5e4249fda16
Peter Rosario <PeterRosario@ksu.edu>
2018-05-15 14:40:01 -0500

    EntryModels: Added.

---
+++ app/src/main/java/org/wheatgenetics/coordinate/model/ExcludedEntryModel.java
@@ -0,0 +1,46 @@
+package org.wheatgenetics.coordinate.model;
+
+/**
+ * Uses:
+ * android.support.annotation.IntRange
+ *
+ * org.wheatgenetics.coordinate.R
+ *
+ * org.wheatgenetics.coordinate.model.EntryModel
+ * org.wheatgenetics.coordinate.model.IncludedEntryModel
+ */
+public class ExcludedEntryModel extends org.wheatgenetics.coordinate.model.EntryModel
+{
+    public static final java.lang.String DATABASE_VALUE = "excluded";
+
+    // region Constructors
+    ExcludedEntryModel(
+    @android.support.annotation.IntRange(from = 1) final long gridId,
+    @android.support.annotation.IntRange(from = 1) final int  row   ,
+    @android.support.annotation.IntRange(from = 1) final int  col   ) { super(gridId, row, col); }
+
+    public ExcludedEntryModel(
+    @android.support.annotation.IntRange(from = 1) final long id       ,
+    @android.support.annotation.IntRange(from = 1) final long gridId   ,
+    @android.support.annotation.IntRange(from = 1) final int  row      ,
+    @android.support.annotation.IntRange(from = 1) final int  col      ,
+                                                   final long timestamp)
+    { super(id, gridId, row, col, timestamp); }
+
+    public ExcludedEntryModel(
+    final org.wheatgenetics.coordinate.model.IncludedEntryModel includedEntryModel)
+    { super(includedEntryModel); }
+    // endregion
+
+    // region Overridden Methods
+    @java.lang.Override        java.lang.String getSeedExportValue       () { return "exclude"; }
+    @java.lang.Override        java.lang.String getUserDefinedExportValue() { return "exclude"; }
+    @java.lang.Override public java.lang.String getValue                 () { return null     ; }
+
+    @java.lang.Override public java.lang.String getDatabaseValue()
+    { return org.wheatgenetics.coordinate.model.ExcludedEntryModel.DATABASE_VALUE; }
+
+    @java.lang.Override public int backgroundResource()
+    { return org.wheatgenetics.coordinate.R.drawable.excluded_entry; }
+    // endregion
+}
