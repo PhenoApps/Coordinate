a30cd73b7c86d7ddd892f4f03c8079f6b296c0d0
Peter Rosario <PeterRosario@ksu.edu>
2018-05-17 15:45:52 -0500

    PreferenceFragment: Added.

---
+++ app/src/main/java/org/wheatgenetics/coordinate/nisl/PreferenceFragment.java
@@ -0,0 +1,21 @@
+package org.wheatgenetics.coordinate.nisl;
+
+/**
+ * Uses:
+ * android.os.Bundle
+ * android.preference.Fragment
+ * android.support.annotation.Nullable
+ *
+ * org.wheatgenetics.coordinate.R
+ */
+public class PreferenceFragment extends android.preference.PreferenceFragment
+{
+    @java.lang.Override public void onCreate(
+    @android.support.annotation.Nullable final android.os.Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource.
+        this.addPreferencesFromResource(org.wheatgenetics.coordinate.R.xml.preferences);
+    }
+}
