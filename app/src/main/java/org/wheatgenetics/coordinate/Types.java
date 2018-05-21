1da4cd4823c6771905f47c6956a81a179920f0b4
Peter Rosario <PeterRosario@ksu.edu>
2018-05-21 18:17:31 -0500

    MainActivity: Changed for Coordinate.

--- app/src/main/java/org/wheatgenetics/coordinate/Types.java
+++ app/src/main/java/org/wheatgenetics/coordinate/Types.java
@@ -7,7 +7,7 @@ package org.wheatgenetics.coordinate;
 @java.lang.SuppressWarnings({"ClassExplicitlyExtendsObject"})
 public class Types extends java.lang.Object
 {
-    private static final int CREATE_TEMPLATE = 10, IMPORT_TEMPLATE = 20, CREATE_GRID = 30;
+    static final int CREATE_TEMPLATE = 10, IMPORT_TEMPLATE = 20, CREATE_GRID = 30;
 
     @android.support.annotation.IntDef({
         org.wheatgenetics.coordinate.Types.CREATE_TEMPLATE,
