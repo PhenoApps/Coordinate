093260f8ba44b02b5ec0a0ae42d6cfb56e13cc22
Peter Rosario <PeterRosario@ksu.edu>
2018-05-22 16:44:25 -0500

    PreferenceActivity: Changed toolbar from app name to back arrow.

--- app/src/main/java/org/wheatgenetics/coordinate/nisl/PreferenceActivity.java
+++ app/src/main/java/org/wheatgenetics/coordinate/nisl/PreferenceActivity.java
@@ -5,7 +5,9 @@ package org.wheatgenetics.coordinate.nisl;
  * android.R
  * android.os.Bundle
  * android.support.annotation.Nullable
+ * android.support.v7.app.ActionBar
  * android.support.v7.app.AppCompatActivity
+ * android.view.MenuItem
  *
  * org.wheatgenetics.coordinate.nisl.PreferenceFragment
  */
@@ -16,8 +18,21 @@ public class PreferenceActivity extends android.support.v7.app.AppCompatActivity
     {
         super.onCreate(savedInstanceState);
 
+        {
+            final android.support.v7.app.ActionBar supportActionBar =
+                this.getSupportActionBar();
+            if (null != supportActionBar)
+            {
+                supportActionBar.setTitle                 (/* title        => */ null);
+                supportActionBar.setDisplayHomeAsUpEnabled(/* showHomeAsUp => */ true);
+            }
+        }
+
         // Display PreferenceFragment as the main content.
         this.getFragmentManager().beginTransaction().replace(android.R.id.content,
             new org.wheatgenetics.coordinate.nisl.PreferenceFragment()).commit();
     }
+
+    @java.lang.Override public boolean onOptionsItemSelected(final android.view.MenuItem item)
+    { this.finish(); return super.onOptionsItemSelected(item); }
 }
