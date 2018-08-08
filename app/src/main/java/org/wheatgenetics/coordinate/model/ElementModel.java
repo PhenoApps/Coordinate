08bc4e4883639c16fcc96f697cf7acf6e603f8da
Peter Rosario <PeterRosario@ksu.edu>
2018-08-08 12:26:52 -0500

    Cells: Added.

---
+++ app/src/main/java/org/wheatgenetics/coordinate/model/ElementModel.java
@@ -0,0 +1,4 @@
+package org.wheatgenetics.coordinate.model;
+
+@java.lang.SuppressWarnings({"UnnecessaryInterfaceModifier"}) public interface ElementModel
+{ public abstract int getRowValue(); public abstract int getColValue(); }
