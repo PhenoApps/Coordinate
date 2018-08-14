2763072669ecefc3c92c1b17369c3b4627f25bfc
Peter Rosario <PeterRosario@ksu.edu>
2018-08-14 16:53:12 -0500

    TemplateDisplayFragment: Added.

---
+++ app/src/main/java/org/wheatgenetics/coordinate/tc/TemplateDisplayFragment.java
@@ -0,0 +1,66 @@
+package org.wheatgenetics.coordinate.tc;
+
+/**
+ * Uses:
+ * android.content.Context
+ * android.support.annotation.IntRange
+ *
+ * org.wheatgenetics.coordinate.model.Cell
+ * org.wheatgenetics.coordinate.model.ElementModel
+ *
+ * org.wheatgenetics.coordinate.DisplayFragment
+ * org.wheatgenetics.coordinate.DisplayFragment.Handler
+ *
+ * org.wheatgenetics.coordinate.tc.TemplateElement.Handler
+ * org.wheatgenetics.coordinate.tc.TemplateElements
+ */
+public class TemplateDisplayFragment extends org.wheatgenetics.coordinate.DisplayFragment
+implements org.wheatgenetics.coordinate.tc.TemplateElement.Handler
+{
+    @java.lang.SuppressWarnings({"UnnecessaryInterfaceModifier"})
+    public interface Handler extends org.wheatgenetics.coordinate.DisplayFragment.Handler
+    { public abstract boolean isExcluded(org.wheatgenetics.coordinate.model.Cell cell); }
+
+    public TemplateDisplayFragment() { /* Required empty public constructor. */ }
+
+    // region Overridden Methods
+    @java.lang.Override protected boolean setHandler(final android.content.Context context)
+    {
+        final boolean success;
+
+        if (context instanceof org.wheatgenetics.coordinate.tc.TemplateDisplayFragment.Handler)
+        {
+            this.handler =
+                (org.wheatgenetics.coordinate.tc.TemplateDisplayFragment.Handler) context;
+            success = true;
+        }
+        else success = false;
+
+        return success;
+    }
+
+    @java.lang.Override protected void allocateElements(
+    @android.support.annotation.IntRange(from = 1) final int lastRow,
+    @android.support.annotation.IntRange(from = 1) final int lastCol)
+    {
+        if (null == this.elements)
+            this.elements = new org.wheatgenetics.coordinate.tc.TemplateElements(
+                this.getActivity(), lastRow, lastCol,this);
+        else
+            this.elements.allocate(lastRow, lastCol);
+    }
+
+    // region org.wheatgenetics.coordinate.tc.TemplateElement.Handler Overridden Methods
+    @java.lang.Override
+    public void toggle(final org.wheatgenetics.coordinate.model.ElementModel elementModel)
+    { super.toggle(elementModel); }
+
+    @java.lang.Override
+    public boolean isExcluded(final org.wheatgenetics.coordinate.model.Cell cell)
+    {
+        return ((org.wheatgenetics.coordinate.tc.TemplateDisplayFragment.Handler)
+            this.handler).isExcluded(cell);
+    }
+    // endregion
+    // endregion
+}
