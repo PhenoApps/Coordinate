70bfbb8c15a65075f067a3a81cc807b691a9a705
Peter Rosario <PeterRosario@ksu.edu>
2018-08-22 14:37:20 -0500

    menu/main.xml: Deleted for Coordinate.

--- app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
@@ -61,11 +61,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    { return true; }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
@@ -76,11 +72,6 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
