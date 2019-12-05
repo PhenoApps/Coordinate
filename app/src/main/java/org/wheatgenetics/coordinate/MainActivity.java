79b24979142c6821b08471571b2105fac5507830
Peter Rosario <PeterRosario@ksu.edu>
2019-12-05 14:01:46 -0600

    MainActivity: Let app bar title be set to app name.

--- app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
@@ -4,7 +4,6 @@ package org.wheatgenetics.coordinate;
  * Uses:
  * android.os.Bundle
  *
- * androidx.appcompat.app.ActionBar
  * androidx.appcompat.app.AppCompatActivity
  * androidx.appcompat.widget.Toolbar
  *
@@ -17,16 +16,10 @@ public class MainActivity extends androidx.appcompat.app.AppCompatActivity
         super.onCreate(savedInstanceState);
         this.setContentView(org.wheatgenetics.coordinate.R.layout.activity_main);
 
-        // region Configure action bar.
         {
             final androidx.appcompat.widget.Toolbar toolbar = this.findViewById(
                 org.wheatgenetics.coordinate.R.id.toolbar);         // From layout/app_bar_main.xml.
             this.setSupportActionBar(toolbar);
         }
-        {
-            final androidx.appcompat.app.ActionBar supportActionBar = this.getSupportActionBar();
-            if (null != supportActionBar) supportActionBar.setTitle(null);
-        }
-        // endregion
     }
 }
