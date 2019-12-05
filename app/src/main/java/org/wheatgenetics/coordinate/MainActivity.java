e51ad4d92aed9b3fdbaa275b741a22ed5e9cf3c6
Peter Rosario <PeterRosario@ksu.edu>
2019-12-05 11:28:50 -0600

    MainActivity: Added app bar.

--- app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
@@ -4,7 +4,9 @@ package org.wheatgenetics.coordinate;
  * Uses:
  * android.os.Bundle
  *
+ * androidx.appcompat.app.ActionBar
  * androidx.appcompat.app.AppCompatActivity
+ * androidx.appcompat.widget.Toolbar
  *
  * org.wheatgenetics.coordinate.R
  */
@@ -14,5 +16,17 @@ public class MainActivity extends androidx.appcompat.app.AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         this.setContentView(org.wheatgenetics.coordinate.R.layout.activity_main);
+
+        // region Configure action bar.
+        {
+            final androidx.appcompat.widget.Toolbar toolbar = this.findViewById(
+                org.wheatgenetics.coordinate.R.id.toolbar);         // From layout/app_bar_main.xml.
+            this.setSupportActionBar(toolbar);
+        }
+        {
+            final androidx.appcompat.app.ActionBar supportActionBar = this.getSupportActionBar();
+            if (null != supportActionBar) supportActionBar.setTitle(null);
+        }
+        // endregion
     }
 }
