c28a18080d9cd21c93fb4859ff1a27210cfd61b1
Peter Rosario <PeterRosario@ksu.edu>
2019-12-05 09:28:54 -0600

    MainActivity: Added and replaced OldMainActivity.

---
+++ app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
@@ -0,0 +1,18 @@
+package org.wheatgenetics.coordinate;
+
+/**
+ * Uses:
+ * android.os.Bundle
+ *
+ * androidx.appcompat.app.AppCompatActivity
+ *
+ * org.wheatgenetics.coordinate.R
+ */
+public class MainActivity extends androidx.appcompat.app.AppCompatActivity
+{
+    @java.lang.Override protected void onCreate(final android.os.Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.setContentView(org.wheatgenetics.coordinate.R.layout.activity_main);
+    }
+}
