e0e4de3413c04253851279ada249ea2a774a0532
Peter Rosario <PeterRosario@ksu.edu>
2019-12-09 14:36:35 -0600

    OldMainActivity, Consts: Moved two private constants from OldMainActivity to Consts.

---
+++ app/src/main/java/org/wheatgenetics/coordinate/Consts.java
@@ -0,0 +1,7 @@
+package org.wheatgenetics.coordinate;
+
+@java.lang.SuppressWarnings({"ClassExplicitlyExtendsObject"}) class Consts extends java.lang.Object
+{
+    static final java.lang.String COORDINATE_DIR_NAME = "Coordinate",
+        BLANK_HIDDEN_FILE_NAME = ".coordinate";
+}
