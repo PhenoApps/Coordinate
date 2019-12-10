b654eefd524d1dcaddb7d556a651d73907f1e148
Peter Rosario <PeterRosario@ksu.edu>
2019-12-10 10:05:37 -0600

    MainActivity: Made Template menu item work.

--- app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/coordinate/MainActivity.java
@@ -2,26 +2,47 @@ package org.wheatgenetics.coordinate;
 
 /**
  * Uses:
+ * android.app.Activity
  * android.content.Intent
  * android.os.Bundle
  * android.view.Menu
+ * android.view.MenuItem
  * android.view.View
  * android.widget.AdapterView<>
  * android.widget.AdapterView.OnItemClickListener
  * android.widget.ArrayAdapter
  * android.widget.ListView
  *
+ * androidx.annotation.NonNull
  * androidx.appcompat.app.AppCompatActivity
+ *
+ * org.wheatgenetics.androidlibrary.Utils
+ *
+ * org.wheatgenetics.coordinate.database.TemplatesTable
+ *
+ * org.wheatgenetics.coordinate.model.TemplateModel
  *
  * org.wheatgenetics.coordinate.templates.TemplatesActivity
  *
+ * org.wheatgenetics.coordinate.tc.TemplateCreator
+ * org.wheatgenetics.coordinate.tc.TemplateCreator.Handler
+ *
  * org.wheatgenetics.coordinate.R
+ * org.wheatgenetics.coordinate.Types
  */
 public class MainActivity extends androidx.appcompat.app.AppCompatActivity
+implements org.wheatgenetics.coordinate.tc.TemplateCreator.Handler
 {
-    private android.content.Intent templatesIntentInstance = null;                      // lazy load
+    // region Fields
+    private android.content.Intent                               templatesIntentInstance = null;//ll
+    private org.wheatgenetics.coordinate.database.TemplatesTable templatesTableInstance  = null;//ll
+    private org.wheatgenetics.coordinate.tc.TemplateCreator      templateCreator         = null;//ll
+    // endregion
 
     // region Private Methods
+    private void showLongToast(final java.lang.String text)
+    { org.wheatgenetics.androidlibrary.Utils.showLongToast(this, text); }
+
     private android.content.Intent templatesIntent()
     {
         if (null == this.templatesIntentInstance) this.templatesIntentInstance =
@@ -31,6 +52,14 @@ public class MainActivity extends androidx.appcompat.app.AppCompatActivity
     }
 
     private void startTemplatesActivity() { this.startActivity(this.templatesIntent()); }
+
+    @androidx.annotation.NonNull
+    private org.wheatgenetics.coordinate.database.TemplatesTable templatesTable()
+    {
+        if (null == this.templatesTableInstance) this.templatesTableInstance =
+            new org.wheatgenetics.coordinate.database.TemplatesTable(this);
+        return this.templatesTableInstance;
+    }
     // endregion
 
     // region Overridden Methods
@@ -68,5 +97,37 @@ public class MainActivity extends androidx.appcompat.app.AppCompatActivity
         this.getMenuInflater().inflate(org.wheatgenetics.coordinate.R.menu.menu_main, menu);
         return true;
     }
+
+    @java.lang.Override protected void onActivityResult(final int requestCode,
+    final int resultCode, final android.content.Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (android.app.Activity.RESULT_OK == resultCode && null != data)
+            switch (requestCode)
+            {
+                case org.wheatgenetics.coordinate.Types.CREATE_TEMPLATE:
+                    if (null != this.templateCreator)
+                        this.templateCreator.setExcludedCells(data.getExtras());
+                    break;
+            }
+    }
+
+    // region org.wheatgenetics.coordinate.tc.TemplateCreator.Handler Overridden Method
+    @java.lang.Override public void handleTemplateCreated(@androidx.annotation.NonNull
+    final org.wheatgenetics.coordinate.model.TemplateModel templateModel)
+    {
+        final java.lang.String not = this.templatesTable().insert(templateModel) > 0 ? "" : " not";
+        this.showLongToast(templateModel.getTitle() + not + " created");
+    }
     // endregion
+    // endregion
+
+    public void onTemplateMenuItemClick(final android.view.MenuItem menuItem)
+    {
+        if (null == this.templateCreator)
+            this.templateCreator = new org.wheatgenetics.coordinate.tc.TemplateCreator(
+                this, org.wheatgenetics.coordinate.Types.CREATE_TEMPLATE,this);
+        this.templateCreator.create();
+    }
 }
