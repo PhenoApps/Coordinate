3900985f46f7b8454de3ddf55e32a84b5a0fd867
Peter Rosario <PeterRosario@ksu.edu>
2020-01-16 09:42:47 -0600

    Fixed mistake in last commit (I put the changed files in the wrong directory).

--- app/src/main/java/org/wheatgenetics/coordinate/gc/ts/TemplateChoiceAlertDialog.java
+++ app/src/main/java/org/wheatgenetics/coordinate/gc/ts/TemplateChoiceAlertDialog.java
@@ -22,7 +22,19 @@ class TemplateChoiceAlertDialog extends org.wheatgenetics.androidlibrary.AlertDi
     @androidx.annotation.StringRes private final int firstItem;
     @androidx.annotation.NonNull   private final
         org.wheatgenetics.coordinate.gc.ts.TemplateChoiceAlertDialog.Handler handler;
+
+    @java.lang.SuppressWarnings({"CStyleArrayDeclaration"})
+    private int                                             itemsInstance[]         = null;    // ll
+    private android.content.DialogInterface.OnClickListener onClickListenerInstance = null;    // ll
     // endregion
+
+    // region Private Methods
+    @androidx.annotation.NonNull private int[] items()
+    {
+        if (null == this.itemsInstance) this.itemsInstance = new int[]{this.firstItem,
+            org.wheatgenetics.coordinate.R.string.TemplateChoiceAlertDialogNewItem};
+        return this.itemsInstance;
+    }
 
     private void choose(final int which)
     {
@@ -33,24 +45,30 @@ class TemplateChoiceAlertDialog extends org.wheatgenetics.androidlibrary.AlertDi
         }
     }
 
+    @androidx.annotation.NonNull
+    private android.content.DialogInterface.OnClickListener onClickListener()
+    {
+        if (null == this.onClickListenerInstance) this.onClickListenerInstance =
+            new android.content.DialogInterface.OnClickListener()
+            {
+                @java.lang.Override public void onClick(
+                final android.content.DialogInterface dialog, final int which)
+                { org.wheatgenetics.coordinate.gc.ts.TemplateChoiceAlertDialog.this.choose(which); }
+            };
+        return this.onClickListenerInstance;
+    }
+    // endregion
+
     TemplateChoiceAlertDialog(final android.app.Activity activity,
     @androidx.annotation.StringRes final int firstItem, @androidx.annotation.NonNull
     final org.wheatgenetics.coordinate.gc.ts.TemplateChoiceAlertDialog.Handler handler)
     { super(activity); this.firstItem = firstItem; this.handler = handler; }
 
+    // region Overridden Methods
     @java.lang.Override public void configure()
-    {
-        this.setTitle(org.wheatgenetics.coordinate.R.string.TemplateChoiceAlertDialogTitle)
-            .setItems(new int[]{this.firstItem,
-                    org.wheatgenetics.coordinate.R.string.TemplateChoiceAlertDialogNewItem},
-                new android.content.DialogInterface.OnClickListener()
-                {
-                    @java.lang.Override public void onClick(
-                    final android.content.DialogInterface dialog, final int which)
-                    {
-                        org.wheatgenetics.coordinate.gc.ts.TemplateChoiceAlertDialog.this.choose(
-                            which);
-                    }
-                });
-    }
+    { this.setTitle(org.wheatgenetics.coordinate.R.string.TemplateChoiceAlertDialogTitle); }
+
+    @java.lang.Override public void show()
+    { this.setItems(this.items(), this.onClickListener()); this.createShow(); }
+    // endregion
 }
