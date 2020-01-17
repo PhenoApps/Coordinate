4cea9e42eb9f3744b3e81f1d94403987e4110532
Peter Rosario <PeterRosario@ksu.edu>
2020-01-17 14:17:50 -0600

    Improved code.

--- app/src/main/java/org/wheatgenetics/coordinate/grids/ProjectGridsAdapter.java
+++ app/src/main/java/org/wheatgenetics/coordinate/grids/ProjectGridsAdapter.java
@@ -17,7 +17,7 @@ package org.wheatgenetics.coordinate.grids;
 class ProjectGridsAdapter extends org.wheatgenetics.coordinate.grids.GridsAdapter
 {
     // region Fields
-    @androidx.annotation.IntRange(from = 1) private long projectId;
+    @androidx.annotation.IntRange(from = 1) private final long projectId;
     private org.wheatgenetics.coordinate.model.BaseJoinedGridModels
         baseJoinedGridModelsInstance = null;                                            // lazy load
     // endregion
