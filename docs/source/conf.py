c38ebb3f264c34d2fe8d79c1fd842f6dfd0852cb
Trevor Rife <trife@users.noreply.github.com>
2022-04-13 04:08:12 -0500

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,10 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "img/icon.png"
+html_theme_options = {
+    "logo_only": False
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
