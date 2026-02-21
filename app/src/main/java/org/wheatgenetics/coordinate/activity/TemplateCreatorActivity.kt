54e6a2c9e220a33ce8f00dbd866155ee513e342b
trife <twrife@clemson.edu>
2026-02-21 07:35:35 -0500

    template creator updates

--- app/src/main/java/org/wheatgenetics/coordinate/activity/TemplateCreatorActivity.kt
+++ app/src/main/java/org/wheatgenetics/coordinate/activity/TemplateCreatorActivity.kt
@@ -1,7 +1,6 @@
 package org.wheatgenetics.coordinate.activity
 
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -26,9 +25,7 @@ class TemplateCreatorActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         val rootView = window.decorView.findViewById<android.view.View>(android.R.id.content)
-        InsetHandler.setupStandardInsets(rootView, toolbar)
-
-        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        InsetHandler.setupStandardInsetsWithIme(rootView, toolbar)
 
         supportActionBar?.apply {
             title = if (intent?.hasExtra(TEMPLATE_EDIT) == true)
